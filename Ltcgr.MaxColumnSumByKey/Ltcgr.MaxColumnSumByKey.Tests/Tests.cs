048c776c266c03d708094e42f513878232b943ca
Jeffrey Fuller <jjafuller@gmail.com>
2017-08-06 14:38:23 -0400

    recreate project with vs2017

--- Ltcgr.MaxColumnSumByKey/Ltcgr.MaxColumnSumByKey.Tests/Tests.cs
+++ Ltcgr.MaxColumnSumByKey/Ltcgr.MaxColumnSumByKey.Tests/Tests.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 
 namespace Ltcgr.MaxColumnSumByKey.Tests
 {
     public class Tests
     {
         [Fact]
-        public void Test()
+        public void Calculate_Returns2006and22569013_Given1and2()
         {
             Assert.True(true);
         }
