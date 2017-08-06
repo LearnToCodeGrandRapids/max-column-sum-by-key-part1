4814107e883d8e3f3fbde4a01869737a2acbc890
Jeffrey Fuller <jjafuller@gmail.com>
2017-08-06 10:14:32 -0400

    Very basic skeleton with base project and test project

---
+++ Ltcgr.MaxColumnSumByKey/Ltcgr.MaxColumnSumByKey.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Ltcgr.MaxColumnSumByKey.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
